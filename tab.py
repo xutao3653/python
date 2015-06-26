6018c1e8b24c64e963e764c1b48a0077b8409bf6
root <root@localhost.localdomain>
2015-06-26 14:05:54 +0800

    test

---
+++ tab.py
@@ -0,0 +1,10 @@
+# python startup file
+import sys
+import readline
+import rlcompleter
+import atexit
+import os
+# tab completion
+readline.parse_and_bind('tab: complete')
+# history file
+histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
