30f609384aaf8d77a054fceefb0459d9818c92ab
xutao3653@gmail.com <xutao3653@gmail.com>
2015-06-30 09:40:57 +0800

    add mail

---
+++ SendMail.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+import smtplib
+from email.mime.text import MIMEText
+mailto_list=['15210871139@139.com']
+mail_host="smtp.mxhichina.com"
+mail_user="alert@liuker.cn"
+mail_pass="qwer,1234"
+mail_postfix="liuker.cn"
+
+def send_mail(to_list,sub,content):
+    me="hello"+"<"+mail_user+"@"+mail_postfix+">"
+    msg = MIMEText(content,_subtype='plain',_charset='utf-8')
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = ";".join(to_list)
+    try:
+        server = smtplib.SMTP()
+        server.connect(mail_host)
+        server.login(mail_user,mail_pass)
+        server.sendmail(me, to_list, msg.as_string())
+        server.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
+if __name__ == '__main__':
+    if send_mail(mailto_list,"This is test mail.","hello world!"):
+        print "发送成功"
+    else:
+        print "发送失败"
